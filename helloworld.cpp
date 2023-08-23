79126a654ae9160fc9d47e4a20cb1d28bff2f2b7
Joey Banowetz <62677833+MagneticStar@users.noreply.github.com>
2023-08-23 15:34:45 -0500

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,17 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <string>
+# include<stdio.h>
 
-using namespace std;
-
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+int main(void) {
+    return 0;
 }
-// https://code.visualstudio.com/docs/cpp/config-mingw
