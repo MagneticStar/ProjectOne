c6f37b0204f2bd148d24c520ef322699ea0bed16
codeahl <142953446+codeahl@users.noreply.github.com>
2023-08-30 20:40:11 -0500

    methods

--- And.java
+++ And.java
@@ -1,6 +1,25 @@
 public class And extends Gate {
-    
-    public And(byte andGate) {
+    private boolean boolIn1;
+    private boolean boolIn2;
+    private boolean boolOut;
+
+    public And() {
         super();
+    }
+
+    public boolean evaluate(){
+        return boolIn1 && boolIn2;
+    }
+
+    public void setBool1(boolean bool1) {
+        boolIn1 = bool1;
+    }
+
+    public void setBool2(boolean bool2) {
+        boolIn2 = bool2;
+    }
+
+    public boolean getBoolOut() {
+        return boolOut;
     }
 }
