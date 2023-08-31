99a0a47dd29d806dca40bc5d64e576faa5f8b849
StyleTechInnovations <140669997+StyleTechInnovations@users.noreply.github.com>
2023-08-30 20:58:50 -0500

    @override

--- Gate.java
+++ Gate.java
@@ -3,9 +3,15 @@ public class Gate {
     public Gate() {
         
     }
-    
-
-
+    public void evaluate(){
+    }
+    public void setBoolIn1(boolean bool1) {
+    }
+    public void setBoolIn2(boolean bool2) {
+    }
+    public boolean getBoolOut() {
+        return false;
+    }
 
     
 }
